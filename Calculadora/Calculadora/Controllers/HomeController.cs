422d2c5fa4243c4f0e233657461f0b88bc4ac5a4
Joao Alves <aluno19262@ipt.pt>
2019-03-22 20:20:04 +0000

    adiçao do codigo do controller para processar os dados da calculadora

--- Calculadora/Calculadora/Controllers/HomeController.cs
+++ Calculadora/Calculadora/Controllers/HomeController.cs
@@ -13,5 +13,37 @@ namespace Calculadora.Controllers
         {
             return View();
         }
+
+        // Post: Home
+        [HttpPost]
+        public ActionResult Index(string visor,string bt)
+        {
+            switch (bt)
+            {
+                case "0":
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                    if (visor == "0")
+                    {
+                        visor = bt;
+                    }
+                    else
+                    {
+                        visor += bt;
+                    }
+
+                    break;
+              
+            }
+            ViewBag.Resposta = visor;
+            return View();
+        }
     }
 }
